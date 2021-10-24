bca759e0608aace7a75e691d86da8695a0187002
Quoc Nguyen <charlesivnguyen4@yahoo.com>
2021-10-24 15:40:38 -0500

    Add logo to NavBar

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -11,43 +11,60 @@ import p6 from '../Media/p6.png';
 import p7 from '../Media/p7.png';
 import p8 from '../Media/p8.png';
 import p9 from '../Media/p9.png';
+import logo from '../Media/logo_tran.png';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import HomeIcon from '@mui/icons-material/Home';
 import FastfoodIcon from '@mui/icons-material/Fastfood';
 import StoreIcon from '@mui/icons-material/Store';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 //#endregion
 
 function Home() {
     const [value, setValue] = React.useState(0);
 
     const handleChange = (event, newValue) => {
-      setValue(newValue);
+        setValue(newValue);
     };
 
     return (
         <div className='h-screen w-screen overflow-x-hidden overflow-y-scroll flex-col justify-center bg-gradient-to-r from-yellow-200 via-pink-100 to-yellow-100'>
-            <div className='flex justify-end'>
             {NavBar(value, handleChange)}
-            </div>
             <div className=" flex justify-center my-12">
-                <Box className='w-10/12 sm:w-2/5 h-auto'>
+                <Box className='w-10/12 sm:w-4/12 h-auto'>
                     {InstagramStyledImgGroup()}
                 </Box>
             </div>
-
         </div>
-
     );
 }
 export default Home;
 
+
+
 //#region components
 function NavBar(value, handleChange) {
+    return <Grid container rowSpacing={0.3} columnSpacing={0.3} justifyContent="center" alignItems="center">
+        <Grid item xs={4} md={1} className='spacer' />
+        <Grid item xs={4} md={3} className=''>
+            <img src={logo} className='p-0' alt="logo" className="w-24 h-auto" />
+        </Grid>
+        <Grid item xs={4} md={4} className='spacer' />
+        <Grid item xs={4} md={3} className=''>
+            <div className='flex justify-end'>
+                {NavBarMenu(value, handleChange)}
+            </div>
+        </Grid>
+        <Grid item xs={4} md={1} className='spacer' />
+    </Grid>;
+}
+
+function NavBarMenu(value, handleChange) {
     return <Tabs value={value} onChange={handleChange} aria-label="icon label tabs example">
         <Tab icon={<HomeIcon />} label="HOME" />
         <Tab icon={<FastfoodIcon />} label="DRINK" />
         <Tab icon={<StoreIcon />} label="MERCHANDISE" />
+        <Tab icon={<ShoppingCartIcon />} label="ORDER" />
     </Tabs>;
 }
 
